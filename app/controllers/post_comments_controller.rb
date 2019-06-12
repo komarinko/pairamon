7a04822fa418403301ff111b2251776cec5f9719
komarinko <keserapaseri8214@gmail.com>
2019-06-12 16:19:41 +0900

    save2

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,6 +3,7 @@ class PostCommentsController < ApplicationController
 	    user = User.find(params[:user_id])
 	    comment = current_user.post_comments.new(post_comment_params)
 	    comment.user_id = user.id
+	    comment.post_image_id = current_user.id
 	    comment.save
 	    redirect_to user_path(user.id)
 	end
